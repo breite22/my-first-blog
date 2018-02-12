9f1741f184c64bb89d9bbe96c57c1937181ea94a
breite22 <breite22@uwm.edu>
2018-02-11 18:26:53 -0600

    Adding more to site, securing site, and creating comment model.

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# This line redirects a successful login
+# to the top-level index
+
+LOGIN_REDIRECT_URL = '/'
